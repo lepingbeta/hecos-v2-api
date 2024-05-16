4ec9a48f2e9bf4734d87361b696786f38884c779
Symphony <zhangleping@cezhiqiu.com>
2024-05-16 14:41:28 +0800

    创建时加入更新时间

--- services/config/CreateConfig/CreateConfigPre.go
+++ services/config/CreateConfig/CreateConfigPre.go
@@ -1,3 +1,13 @@
+/*
+ * @Author       : Symphony zhangleping@cezhiqiu.com
+ * @Date         : 2024-05-15 20:05:45
+ * @LastEditors  : Symphony zhangleping@cezhiqiu.com
+ * @LastEditTime : 2024-05-16 14:01:45
+ * @FilePath     : /hecos-v2-api/services/config/CreateConfig/CreateConfigPre.go
+ * @Description  :
+ *
+ * Copyright (c) 2024 by 大合前研, All Rights Reserved.
+ */
 package CreateConfig
 
 import (
@@ -15,5 +25,6 @@ import (
 func CreateConfigPre(params t.CreateConfigParams) (bson.D, string, string, error) {
 	bsonD, _ := utils.Struct2BsonD(params)
 	bsonD = append(bsonD, bson.E{Key: "is_delete", Value: 0})
+	bsonD = append(bsonD, bson.E{Key: "update_time", Value: ""})
 	return bsonD, "", "config_create_config_insert_pre_result", nil
 }
