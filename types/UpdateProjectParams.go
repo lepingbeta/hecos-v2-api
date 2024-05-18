8a18e47eabb319b9b4c4af5cad217ddb1c003d74
Symphony <zhangleping@cezhiqiu.com>
2024-05-18 22:17:17 +0800

    加入回调链接

--- types/UpdateProjectParams.go
+++ types/UpdateProjectParams.go
@@ -5,6 +5,8 @@ type UpdateProjectParams struct {
 	// 项目名
 	ProjectName string `bson:"project_name" json:"project_name" validate:"required,min=3,max=50" `
 	// 项目id
-	ProjectId string `bson:"project_id" json:"project_id" validate:"required,mongoId" `
+	ProjectId string `bson:"project_id" json:"project_id" validate:"required,mongoId,findInDb=needExists project _id update_project_project_id_find_in_db_err is_not_delete" `
+	// 修改配置后回调接口，如果有的话
+	UpdateCallback string `bson:"update_callback" json:"update_callback" validate:"" `
 }
 
