aec88cd6cfea039feddc010b80f6006026812d56
Symphony <zhangleping@cezhiqiu.com>
2024-06-14 04:20:49 +0800

    完成QueryOtherCollection

--- services/config/ConfigList/ConfigList.go
+++ services/config/ConfigList/ConfigList.go
@@ -2,80 +2,151 @@ package ConfigList
 
 import (
 	"github.com/gin-gonic/gin"
+	httpTypes "github.com/lepingbeta/go-common-v2-dh-http/types"
 	dhlog "github.com/lepingbeta/go-common-v2-dh-log"
 	mongodb "github.com/lepingbeta/go-common-v2-dh-mongo"
+	utils "github.com/lepingbeta/go-common-v2-dh-utils"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	t "tangxiaoer.shop/dahe/hecos-v2-api/types"
+	// {{占位符 import}}
 )
 
-func ConfigList(params t.ConfigListParams, c *gin.Context) ([]bson.M, string, string, error) {
-
-	filter, msg, msgKey, err := ConfigListPre(params, c)
-	if err != nil {
-		dhlog.Error(err.Error())
-		return nil, msg, msgKey, err
-	}
-
-	result, err := mongodb.FindList("config", filter)
-
-	if err != nil {
-		dhlog.Error(err.Error())
-		return nil, err.Error(), "config_config_list_find_list_error", err
-	}
-
-	// 后置处理器
-	filter, result, msg, msgKey, err = postProcessing(params, filter, result, c)
-	if err != nil {
-		dhlog.Error(err.Error())
-		return nil, msg, msgKey, err
-	}
-
-	finalResult, msg, msgKey, err := ConfigListPost(params, filter, result, c)
-	if err != nil {
-		dhlog.Error(err.Error())
-		return nil, msg, msgKey, err
-	}
-
-	return finalResult, msg, msgKey, err
+type ConfigList struct {
+	Params       t.ConfigListParams // 入参结构体版 （原始版）
+	DataM        bson.M             // 入参bson.M版 (入库用)
+	SliceOfDataM []bson.M           // 入参slice版
+	Filter       bson.M             // 入参bson.M版 (查询用)
+	DataD        bson.D             // 入参bson.D版 (入库用)
+	C            *gin.Context
+	Result       any
+	Msg          string
+	MsgKey       string
+	Err          error
+	FindOpts     *options.FindOptions
+	DocID        primitive.ObjectID
+	// 临时变量3兄弟
+	Temp1 []bson.M
+	Temp2 any
+	Temp3 any
 }
 
-func postProcessing(params t.ConfigListParams, filter bson.M, result []bson.M, c *gin.Context) (bson.M, []bson.M, string, string, error) {
-	filter, result, msg, msgKey, err := findWithOneByOne(params, filter, result, c)
-	if err != nil {
-		return filter, result, msg, msgKey, nil
+func (p *ConfigList) ConfigList() {
+
+	p.CutFilter()
+	if p.Err != nil {
+		return
 	}
-	// {{占位符 postProcessing}}
-	return filter, result, "Post Processing Success", "config_config_list_post_processing_success", nil
+	p.AddDelete()
+	if p.Err != nil {
+		return
+	}
+	p.RemovefEmpty()
+	if p.Err != nil {
+		return
+	}
+	p.FindFields()
+	if p.Err != nil {
+		return
+	}
+	p.GetList()
+	if p.Err != nil {
+		return
+	}
+	p.TempPrepare()
+	if p.Err != nil {
+		return
+	}
+	p.QueryOtherCollection()
+	if p.Err != nil {
+		return
+	} // {{占位符 composition caller}}
 }
 
-func findWithOneByOne(params t.ConfigListParams, filter bson.M, result []bson.M, c *gin.Context) (bson.M, []bson.M, string, string, error) {
+func (p *ConfigList) CutFilter() {
+	p.Filter = mongodb.FilterBsonM(p.Filter, []string{`project_id`})
+	delete(p.DataM, `project_id`)
+}
+
+func (p *ConfigList) AddDelete() {
+	p.Filter[`is_delete`] = 0
+}
+
+func (p *ConfigList) RemovefEmpty() {
+	switch v := p.Filter[`project_id`].(type) {
+	case string:
+		if v == "" {
+			delete(p.Filter, `project_id`)
+		}
+	case primitive.ObjectID:
+		if v.IsZero() {
+			delete(p.Filter, `project_id`)
+		}
+	}
+}
+
+func (p *ConfigList) FindFields() {
+	fieldList := bson.D{{Key: "_id", Value: 1},
+		{Key: "project_id", Value: 1},
+		{Key: "config_name", Value: 1},
+		{Key: "codename", Value: 1},
+		{Key: "use_callback", Value: 1},
+		{Key: "create_time", Value: 1},
+		{Key: "update_time", Value: 1}}
+	p.FindOpts.SetProjection(fieldList)
+}
+
+func (p *ConfigList) GetList() {
+	result, err := mongodb.FindList("config", p.Filter, p.FindOpts)
+
+	if err != nil {
+		dhlog.Error(err.Error())
+		p.Msg = utils.DebugMsg("config_config_list_GetList FindList 错误：" + p.Err.Error())
+		p.MsgKey = "config_config_list_GetList_failed"
+		return
+	}
+
+	p.Result = result
+}
+
+func (p *ConfigList) TempPrepare() {
+	method := "slice" // slice,pager
+	switch method {
+	case "bson.M":
+		// 将 bson.M 对象放入切片
+		p.Temp1 = append(p.Temp1, p.Result.(bson.M))
+	case "slice":
+		// 将 bson.M 对象放入切片
+		p.Temp1 = p.Result.([]bson.M)
+	case "pager":
+		p.Temp1 = p.Result.(httpTypes.DataList).List
+	}
+}
+
+func (p *ConfigList) QueryOtherCollection() {
+
 	var sourceValueList []primitive.ObjectID
 	// 遍历切片
-	for _, item := range result {
-		// 使用item["project_id"]来访问每个映射中的project_id字段
-		sourceValue, err := primitive.ObjectIDFromHex(item["project_id"].(string))
-		if err != nil {
-			return filter, result, err.Error(), "config_config_list_findWithOneByOne_source_value_error", nil
-		}
+	for _, item := range p.Temp1 {
+		sourceValue := mongodb.ObjectIDFromHex(item[`project_id`].(string))
 
 		sourceValueList = append(sourceValueList, sourceValue)
 	}
 	filter2 := bson.M{"_id": bson.M{"$in": sourceValueList}}
-	fieldList := bson.D{{"project_name", 1}, {"update_callback", 1}, {"_id", 1}}
+	fieldList := bson.D{{Key: "project_name", Value: 1}, {Key: "update_callback", Value: 1}, {Key: "_id", Value: 1}}
 	// 创建Find选项，设置Projection
 	findOptions := options.Find()
 	findOptions.SetProjection(fieldList)
 	result2, err := mongodb.FindList("project", filter2, findOptions)
 	if err != nil {
-		return filter, result, err.Error(), "config_config_list_findWithOneByOne_source_find_list_error", nil
+		return
 	}
 
 	// 创建一个映射，以project_id为键，以project_name为值
-	idToDetailsMap := make(map[primitive.ObjectID]bson.M)
+	idToDetailsMap := make(map[any]bson.M)
 	for _, res2 := range result2 {
-		keyId := res2["_id"].(primitive.ObjectID)
+		keyId := res2["_id"]
 
 		// 复制res2的内容到新的bson.M映射中
 		details := bson.M{}
@@ -89,20 +160,19 @@ func findWithOneByOne(params t.ConfigListParams, filter bson.M, result []bson.M,
 		idToDetailsMap[keyId] = details
 	}
 
-	for i, item := range result {
-		keyId, _ := primitive.ObjectIDFromHex(item["project_id"].(string))
+	for i, item := range p.Temp1 {
+		keyId := mongodb.ObjectIDFromHex(item[`project_id`].(string))
 		if details, exists := idToDetailsMap[keyId]; exists {
 			// 将details中的字段添加到result[i]中
 			for key, value := range details {
 				// 检查result[i]中是否存在该字段，如果存在则添加
-				if _, exists := result[i][key]; !exists {
-					result[i][key] = value
+				if _, exists := p.Temp1[i][key]; !exists {
+					p.Temp1[i][key] = value
 				}
 			}
 		}
 	}
 
-	return filter, result, "findWithOneByOne查询成功", "config_config_list_findWithOneByOne_success", nil
 }
 
-// {{占位符 processer}}
+// {{占位符 composition}}
